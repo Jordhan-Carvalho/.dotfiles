ac7ac7648cace6e284c93cac0148f5f0594a8420
Jordhan Carvalho <carvalho@jordhan.dev>
2022-07-28 09:30:52 -0300

    feat: add i3 config

--- stow.sh
+++ stow.sh
@@ -3,7 +3,7 @@
 # the -z evaluates to true if the variable is empty
 if [[ -z $STOW_FOLDERS ]]; then
     echo "STOW_FOLDERS is not set"
-    STOW_FOLDERS="nvim,tmux,git,zsh,golang,bin"
+    STOW_FOLDERS="nvim,tmux,git,zsh,golang,bin,i3"
 fi
 
 if [[ -z $DOTFILES ]]; then
